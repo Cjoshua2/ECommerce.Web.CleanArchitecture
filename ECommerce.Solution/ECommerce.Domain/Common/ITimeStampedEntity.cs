165507a31ee3f1ba7055c0083bd6987d2ba6aa71
Christian Joshua Mata <christianjoshua.mata@gmail.com>
2022-10-14 11:12:25 +0800

    some changes

--- ECommerce.Solution/ECommerce.Domain/Common/ITimeStampedEntity.cs
+++ ECommerce.Solution/ECommerce.Domain/Common/ITimeStampedEntity.cs
@@ -1,17 +1,14 @@
-﻿using ECommerce.Domain.Common;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ECommerce.Domain.User
+namespace ECommerce.Domain.Common
 {
-    public class User : ITimeStampedEntity
+    public interface ITimeStampedEntity
     {
         public int Id { get; set; }
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
     }
